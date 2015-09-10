5816ea16425cc2d8b78f4e1894917f41e3beb8ae
spyrbri <spyrbri@gmail.com>
2015-09-10 15:23:27 +0300

    make it modular

--- lib/admin_auto_select.rb
+++ lib/admin_auto_select.rb
@@ -1,44 +1,42 @@
 require "admin_auto_select/version"
 
 module AutoSelectable
-  def auto_select(field, scope = {})
-    create_collection_action(field, @resource, scope)
+  def auto_select(*fields)
+    options = fields.extract_options!
+    create_collection_action(fields, options, @resource)
   end
 
-  def create_collection_action(field, resource, scope = {})
-    collection_action :autocomplete, :method => :get do
+  def create_collection_action(fields, options, resource)
+    collection_action :autoselect, :method => :get do
+      select_fields = "#{resource.table_name}.id, " << fields.join(', ')
       if authorized?(:read, resource)
-        term = ([ params[:id], params[:owner], params[:q]].compact).try(:first)
+        term = ([ params[:id], params[:q]].compact).try(:first)
         term.gsub!('%', '\\\%')
         term.gsub!('_', '\\\_')
         first_term = term.try(:match, /\w \w/) ? (term.split(' '))[0] : term
         page = params[:page].try(:to_i)
-        offset = page ? (page-1)*10+(5*(page-1)) : 0
+        offset = page ? (page - 1) * 10 + (5 * (page - 1)) : 0
+        effective_scope = options[params[:scope]] || options['default_scope'] || resource
+
         if params[:id]
-           user = resource.where('id = ?', params[:id].to_i).select('id, first_name, last_name, email').first
-           render json: user and return
+           first_resource = effective_scope.
+             where("#{resource.table_name}.id = ?", params[:id].to_i).
+             select(select_fields).first
+           render json: first_resource and return
         else
-          effective_scope = scope[params[:scope]] || resource
+          concat_fields = fields.join(" || ' '::text || ")
+          concat_cols = "((#{concat_fields})" <<  " || ' '::text || #{resource.table_name}.id::text)"
 
-          concat_cols = ->(user_table) {
-            "((email || ' '::text || last_name || ' '::text || "\
-            "first_name) || ' '::text || #{user_table}.id::text)"
-          }
+          similarity_sql = ActiveRecord::Base.send(:sanitize_sql_array,
+              ["(similarity(#{resource.table_name}.id::text, :id) + similarity(#{concat_cols}, :id))", id: term])
 
-          subquery = effective_scope.
-            where("#{concat_cols.call(resource.table_name)} ILIKE :term", term: "%#{first_term}%").
-            select("users.id, first_name, last_name, email").to_sql
-
-          similarity_sql =
-            ActiveRecord::Base.send(:sanitize_sql_array,
-              ["(similarity(a.id::text, ?) + similarity(#{concat_cols.call("a")}, ?))",
-                term, term])
-
-          user_records = resource.select('*').from("(#{subquery}) as a")
-          .order("#{similarity_sql} DESC")
-          .limit(15).offset(offset)
+          resource_records = effective_scope.select(select_fields).
+            where("#{concat_cols} ILIKE :term", term: "%#{first_term}%").
+            order("#{similarity_sql} DESC").
+            limit(15).offset(offset)
         end
-        render json: user_records
+
+        render json: resource_records
       end
     end
   end
